de482198ee051be8b97644e1b40911b741b647cb
HazelDaniel <hazeldaniel856@gmail.com>
2022-09-28 12:18:02 +0100

    finished styling the shop page. added desktop support

---
+++ shop-side-pane-mobile.js
@@ -0,0 +1,19 @@
+const SSPMToggler = document.querySelector(".SSPM-toggler");
+const SSPM = document.querySelector(".shop-side-pane-mobile");
+
+
+window.addEventListener("resize", function() {
+	if (this.innerWidth >= 1095) {
+		SSPM.classList.remove("SSPM-closed");
+		
+	}
+})
+SSPMToggler.addEventListener("click", () => {
+if (this.innerWidth < 1095) {
+		console.log("clicking")
+		SSPM.classList.toggle("SSPM-closed");
+	} else {
+		console.log("we just got to larger screen");
+		
+	}
+})
