267e0bd281e8feac63fd62fadb61bc4a9816a4a4
HazelDaniel <hazeldaniel856@gmail.com>
2022-09-29 00:48:09 +0100

    added and styled half of the single product page. will continue when there is power supply

--- shop-side-pane-mobile.js
+++ shop-side-pane-mobile.js
@@ -1,11 +1,12 @@
 const SSPMToggler = document.querySelector(".SSPM-toggler");
 const SSPM = document.querySelector(".shop-side-pane-mobile");
 
-
+if (window.innerWidth >= 1095) {
+	SSPM.classList.remove("SSPM-closed");
+}
 window.addEventListener("resize", function() {
 	if (this.innerWidth >= 1095) {
 		SSPM.classList.remove("SSPM-closed");
-		
 	}
 })
 SSPMToggler.addEventListener("click", () => {
